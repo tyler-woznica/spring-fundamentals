9b31c84cde99d2a26b7c0419294561338490b221
Tyler Woznica <tylerwoznica@outlook.com>
2025-09-25 12:41:00 -0700

    created controller class

---
+++ lab/src/main/java/controller/Controller.java
@@ -0,0 +1,7 @@
+package controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+}
